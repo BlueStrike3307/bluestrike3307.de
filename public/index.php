5dec9ed1a499e7df12c642b80256dd033eec1c06
BlueStrike3307 <blueknight3307@gmail.com>
2020-11-16 20:16:59 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,19 @@
-soon
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title> BlueStrike3307 | Home
+</head>
+<body>
+
+<?php include("includes/design-top.php");?>
+<?php include("includes/navigation.php");?>
+
+<div class="container" id="main-content">
+	#Soon#
+</div>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
